d801b2962d229aa21aebf90f8dd2f8c74dcd40c0
pyan83 <puyanwei@gmail.com>
2017-09-23 18:40:01 +0100

    passing test for adding new restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,19 @@
 class RestaurantsController < ApplicationController
-  # def index
-  #   raise 'hello from the index action'
-  # end
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurants = Restaurant.new
+  end
+
+  def create
+    Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
 end
